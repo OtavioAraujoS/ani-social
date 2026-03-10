a21a950d2f848af764cf989217568ca2df80b869
OtavioAraujoS <otavioaraujo490@gmail.com>
2026-03-10 15:20:47 -0300

    feat: Add unique user name to user schema and implement basic user service with create and find operations.

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -5,6 +5,7 @@ export const roleEnum = pgEnum("role", ["USER", "ADMIN"]);
 export const users = pgTable("users", {
   id: uuid("id").defaultRandom().primaryKey(),
   name: text("name").notNull(),
+  userName: text("user_name").notNull().unique(),
   role: roleEnum("role").default("USER"),
   createdAt: timestamp("created_at").defaultNow(),
 });

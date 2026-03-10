a7e1b981140e584ffa6ccbab876a12ca84ece941
OtavioAraujoS <otavioaraujo490@gmail.com>
2026-03-10 15:34:42 -0300

    feat: Add user management functionality including a new controller, service, and updated user creation schema.

--- src/interfaces/User.ts
+++ src/interfaces/User.ts
@@ -1,4 +1,8 @@
-export interface CreateUser {
-  name: string;
-  userName: string;
-}
+import { t } from "elysia";
+
+export const CreateUserSchema = t.Object({
+  name: t.String(),
+  userName: t.String(),
+});
+
+export type CreateUserInterface = typeof CreateUserSchema.static;

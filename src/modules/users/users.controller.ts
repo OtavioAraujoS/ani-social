78a0b60273c3149fbd2e1b53945041036fe453bb
OtavioAraujoS <otavioaraujo490@gmail.com>
2026-03-10 15:35:08 -0300

    feat: add users controller with routes for fetching all users and creating a new user.

---
+++ src/modules/users/users.controller.ts
@@ -0,0 +1,9 @@
+import Elysia, { t } from "elysia";
+import { UserService } from "./users.service";
+import { CreateUserSchema } from "../../interfaces/User";
+
+export const userController = new Elysia({ prefix: "/users" })
+  .get("/", () => UserService.findAll())
+  .post("/", ({ body }) => UserService.create(body), {
+    body: CreateUserSchema,
+  });

2c9d5c229023d080ee8c399d91b37ff4eef79eb0
OtavioAraujoS <otavioaraujo490@gmail.com>
2026-03-13 14:32:51 -0300

    feat: Implement dedicated password update functionality and adjust HTTP methods for user modifications.

--- src/interfaces/User.ts
+++ src/interfaces/User.ts
@@ -13,10 +13,16 @@ export const UpdateUserSchema = t.Object({
   password: t.String(),
 });
 
+export const UpdateUserPasswordSchema = t.Object({
+  id: t.String(),
+  password: t.String(),
+});
+
 export const DeleteUserSchema = t.Object({
   id: t.String(),
 });
 
 export type CreateUserInterface = typeof CreateUserSchema.static;
 export type UpdateUserInterface = typeof UpdateUserSchema.static;
+export type UpdateUserPasswordInterface = typeof UpdateUserPasswordSchema.static;
 export type DeleteUserInterface = typeof DeleteUserSchema.static;

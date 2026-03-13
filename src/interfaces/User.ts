cf32867fe772776b923197de55b2e502e6b4008b
OtavioAraujoS <otavioaraujo490@gmail.com>
2026-03-13 14:26:49 -0300

    feat: Add user update functionality by introducing a new service method, schema, and controller endpoint.

--- src/interfaces/User.ts
+++ src/interfaces/User.ts
@@ -6,9 +6,17 @@ export const CreateUserSchema = t.Object({
   password: t.String(),
 });
 
+export const UpdateUserSchema = t.Object({
+  id: t.String(),
+  name: t.String(),
+  userName: t.String(),
+  password: t.String(),
+});
+
 export const DeleteUserSchema = t.Object({
   id: t.String(),
 });
 
 export type CreateUserInterface = typeof CreateUserSchema.static;
+export type UpdateUserInterface = typeof UpdateUserSchema.static;
 export type DeleteUserInterface = typeof DeleteUserSchema.static;

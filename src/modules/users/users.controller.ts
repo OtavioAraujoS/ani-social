c7c78bea21ccea0d7c02430028f9ba7ae68c7f20
OtavioAraujoS <otavioaraujo490@gmail.com>
2026-03-13 14:20:06 -0300

    feat: add user deletion endpoint and service method

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,9 +1,13 @@
-import Elysia from "elysia";
+import Elysia, { t } from "elysia";
 import { UserService } from "./users.service";
-import { CreateUserSchema } from "../../interfaces/User";
+import { CreateUserSchema, DeleteUserSchema } from "../../interfaces/User";
 
 export const userController = new Elysia({ prefix: "/users" })
   .get("/", () => UserService.findAll())
   .post("/", ({ body }) => UserService.create(body), {
     body: CreateUserSchema,
+  })
+  .delete("/", ({ body }) => UserService.delete(body), {
+    body: DeleteUserSchema,
   });
+    
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,6 +1,7 @@
+import { eq } from "drizzle-orm";
 import { db } from "../../db";
 import { users } from "../../db/schema";
-import { CreateUserInterface } from "../../interfaces/User";
+import { CreateUserInterface, DeleteUserInterface } from "../../interfaces/User";
 
 export const UserService = {
   findAll: async () => {
@@ -11,4 +12,8 @@ export const UserService = {
     const [newUser] = await db.insert(users).values(data).returning();
     return newUser;
   },
+
+  delete: async ( data: DeleteUserInterface) => {
+    await db.delete(users).where(eq(users.id, data.id));
+  }
 };

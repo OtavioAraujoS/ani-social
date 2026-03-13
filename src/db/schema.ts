d2cb5eb79e8de5cb968beb7fcd1dedfec839e5e2
OtavioAraujoS <otavioaraujo490@gmail.com>
2026-03-13 14:19:42 -0300

    feat: implement user deletion functionality and add password and updatedAt fields to the user schema.

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -6,6 +6,8 @@ export const users = pgTable("users", {
   id: uuid("id").defaultRandom().primaryKey(),
   name: text("name").notNull(),
   userName: text("user_name").notNull().unique(),
+  password: text("password").notNull(),
   role: roleEnum("role").default("USER"),
   createdAt: timestamp("created_at").defaultNow(),
+  updatedAt: timestamp("updated_at").defaultNow(),
 });
--- src/interfaces/User.ts
+++ src/interfaces/User.ts
@@ -3,6 +3,12 @@ import { t } from "elysia";
 export const CreateUserSchema = t.Object({
   name: t.String(),
   userName: t.String(),
+  password: t.String(),
+});
+
+export const DeleteUserSchema = t.Object({
+  id: t.String(),
 });
 
 export type CreateUserInterface = typeof CreateUserSchema.static;
+export type DeleteUserInterface = typeof DeleteUserSchema.static;

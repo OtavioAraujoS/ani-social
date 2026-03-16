3ac6c72972dc3e9bddca240236d985d3c7fe31a6
OtavioAraujoS <otavioaraujo490@gmail.com>
2026-03-16 13:27:59 -0300

    feat: Refactor authentication middleware for reusability and admin role checks, restructure user routes into authenticated and admin-specific groups, and enhance user service with user existence verification.

--- src/modules/auth/auth.middleware.ts
+++ src/modules/auth/auth.middleware.ts
@@ -1,35 +1,39 @@
 import Elysia from "elysia";
 import { jwt } from "@elysiajs/jwt";
 
+export const authPlugin = (app: Elysia) =>
+  app
+    .use(
+      jwt({
+        name: "jwt",
+        secret: process.env.JWT_SECRET || "",
+      }),
+    )
+    .derive(async ({ jwt, headers, set }) => {
+      const authHeader = headers["authorization"];
+      const token = authHeader?.startsWith("Bearer ")
+        ? authHeader.slice(7)
+        : null;
+
+      if (!token) {
+        set.status = 401;
+        throw new Error("Token não fornecido");
+      }
+
+      const payload = await jwt.verify(token);
+
+      if (!payload) {
+        set.status = 401;
+        throw new Error("Token inválido ou expirado");
+      }
+
+      return {
+        user: payload as Record<string, any>,
+      };
+    });
+
 export const protectedRoutes = new Elysia()
-  .use(
-    jwt({
-      name: "jwt",
-      secret: process.env.JWT_SECRET || "",
-    }),
-  )
-  .derive(async ({ jwt, headers, set }) => {
-    const authHeader = headers["authorization"];
-    const token = authHeader?.startsWith("Bearer ")
-      ? authHeader.slice(7)
-      : null;
-
-    if (!token) {
-      set.status = 401;
-      throw new Error("Token não fornecido");
-    }
-
-    const payload = await jwt.verify(token);
-
-    if (!payload) {
-      set.status = 401;
-      throw new Error("Token inválido ou expirado");
-    }
-
-    return {
-      user: payload,
-    };
-  })
+  .use(authPlugin)
   .get("/profile", ({ user }) => {
     return {
       message: "Perfil acessado!",
@@ -37,4 +41,15 @@ export const protectedRoutes = new Elysia()
       role: user.role,
       userName: user.userName,
     };
+  });
+
+export const adminMiddleware = new Elysia()
+  .use(authPlugin)
+  .onBeforeHandle(({ user, set }) => {
+    if (user.role !== "ADMIN") {
+      set.status = 403;
+      throw new Error(
+        "Acesso negado. Apenas administradores podem realizar esta ação.",
+      );
+    }
   });

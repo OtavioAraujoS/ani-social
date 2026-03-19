7ae8bbf40b0441f5964d5f2651a222b7bf3a4d21
OtavioAraujoS <otavioaraujo490@gmail.com>
2026-03-19 13:44:59 -0300

    feat: Add endpoint and service method to retrieve a single anime by ID.

--- src/modules/animes/animes.controller.ts
+++ src/modules/animes/animes.controller.ts
@@ -1,12 +1,17 @@
 import Elysia from "elysia";
 import { authPlugin } from "../auth/auth.middleware";
 import { AnimeService } from "./animes.service";
-import { AnimeListResponseSchema } from "../../interfaces/Anime";
+import { AnimeListResponseSchema, AnimeSchema } from "../../interfaces/Anime";
 
 export const AnimeController = new Elysia({ prefix: "/animes" }).group(
   "",
   (app) =>
-    app.use(authPlugin).get("/", () => AnimeService.findAll(), {
-      response: AnimeListResponseSchema,
-    }),
+    app
+      .use(authPlugin)
+      .get("/", () => AnimeService.findAll(), {
+        response: AnimeListResponseSchema,
+      })
+      .get("/:animeId", ({ params }) => AnimeService.findById(params.animeId), {
+        response: AnimeSchema,
+      }),
 );
--- src/modules/animes/animes.service.ts
+++ src/modules/animes/animes.service.ts
@@ -1,6 +1,10 @@
-import { AnimeListResponseInterface } from "../../interfaces/Anime";
+import {
+  AnimeListResponseInterface,
+  AnimeResponseInterface,
+} from "../../interfaces/Anime";
 import { db } from "../../db";
 import { animes } from "../../db/schema";
+import { eq } from "drizzle-orm";
 
 export const AnimeService = {
   findAll: async (): Promise<AnimeListResponseInterface> => {
@@ -10,6 +14,24 @@ export const AnimeService = {
       throw new Error("Não foi possível verificar os animes - " + error, {
         cause: error,
       });
+    }
+  },
+
+  findById: async (animeId: string): Promise<AnimeResponseInterface> => {
+    try {
+      const [anime] = await db
+        .select()
+        .from(animes)
+        .where(eq(animes.id, animeId));
+      if (!anime) throw new Error("Anime não encontrado");
+      return anime;
+    } catch (error) {
+      throw new Error(
+        "Não foi possível verificar o anime informado - " + error,
+        {
+          cause: error,
+        },
+      );
     }
   },
 };

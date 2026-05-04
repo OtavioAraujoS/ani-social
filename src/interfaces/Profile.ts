a8205a03bc21b7fd57f73f5a5c99ed783a990b0c
OtavioAraujoS <otavioaraujo490@gmail.com>
2026-05-04 10:58:28 -0300

    feat: implement profile interface

---
+++ src/interfaces/Profile.ts
@@ -0,0 +1,50 @@
+import { t } from "elysia";
+
+const AnimeSchema = t.Object({
+  id: t.String(),
+  title: t.String(),
+  episodes: t.Number(),
+  status: t.String(),
+  createdAt: t.Date(),
+  updatedAt: t.Date(),
+  cover: t.Nullable(t.String()),
+});
+
+export const UserProfileAnimesListResponseSchema = t.Array(AnimeSchema);
+
+const TopicSchema = t.Object({
+  id: t.String(),
+  title: t.String(),
+  content: t.String(),
+  createdAt: t.Date(),
+  updatedAt: t.Date(),
+});
+
+export const UserProfileTopicsListResponseSchema = t.Array(TopicSchema);
+
+const CommentSchema = t.Object({
+  id: t.String(),
+  content: t.String(),
+  createdAt: t.Date(),
+  updatedAt: t.Date(),
+});
+
+export const UserProfileCommentsListResponseSchema = t.Array(CommentSchema);
+
+export const UserProfileResponseSchema = t.Object({
+  anime: UserProfileAnimesListResponseSchema,
+  topics: UserProfileTopicsListResponseSchema,
+  comments: UserProfileCommentsListResponseSchema,
+});
+
+export type UserProfileAnimesListResponseInterface =
+  typeof UserProfileAnimesListResponseSchema.static;
+
+export type UserProfileTopicsListResponseInterface =
+  typeof UserProfileTopicsListResponseSchema.static;
+
+export type UserProfileCommentsListResponseInterface =
+  typeof UserProfileCommentsListResponseSchema.static;
+
+export type UserProfileResponseInterface =
+  typeof UserProfileResponseSchema.static;

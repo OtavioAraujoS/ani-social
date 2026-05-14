ea3688ee2bae1d9071af0e424b40f97c8dd7629d
OtavioAraujoS <otavioaraujo490@gmail.com>
2026-05-14 12:52:28 -0300

    feat: add migration to delete orphaned records before enforcing foreign key constraints

--- migrations/0007_tan_karma.sql
+++ migrations/0007_tan_karma.sql
@@ -1,3 +1,10 @@
+DELETE FROM "comments" WHERE "topic_id" NOT IN (SELECT "id" FROM "topics");--> statement-breakpoint
+DELETE FROM "comments" WHERE "created_by_user_id" NOT IN (SELECT "id" FROM "users");--> statement-breakpoint
+DELETE FROM "topics" WHERE "anime_id" NOT IN (SELECT "id" FROM "animes");--> statement-breakpoint
+DELETE FROM "topics" WHERE "created_by_user_id" NOT IN (SELECT "id" FROM "users");--> statement-breakpoint
+DELETE FROM "topics" WHERE "updated_by_user_id" IS NOT NULL AND "updated_by_user_id" NOT IN (SELECT "id" FROM "users");--> statement-breakpoint
+DELETE FROM "animes" WHERE "created_by_user_id" NOT IN (SELECT "id" FROM "users");--> statement-breakpoint
+DELETE FROM "animes" WHERE "updated_by_user_id" IS NOT NULL AND "updated_by_user_id" NOT IN (SELECT "id" FROM "users");--> statement-breakpoint
 ALTER TABLE "animes" ADD CONSTRAINT "animes_created_by_user_id_users_id_fk" FOREIGN KEY ("created_by_user_id") REFERENCES "public"."users"("id") ON DELETE restrict ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "animes" ADD CONSTRAINT "animes_updated_by_user_id_users_id_fk" FOREIGN KEY ("updated_by_user_id") REFERENCES "public"."users"("id") ON DELETE restrict ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "comments" ADD CONSTRAINT "comments_topic_id_topics_id_fk" FOREIGN KEY ("topic_id") REFERENCES "public"."topics"("id") ON DELETE restrict ON UPDATE no action;--> statement-breakpoint
